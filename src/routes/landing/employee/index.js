29a41e7d2e9b0ccfb0b7f24e81dcef8f86e01ab1
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-06 12:16:16 -0800

    landing cleanup

--- src/routes/landing/employee/index.js
+++ src/routes/landing/employee/index.js
@@ -2,13 +2,23 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import styled from "styled-components";
+
 const Employee = () => {
   return (
-    <div>
+    <Container>
       <h1>Employee landing</h1>
       <Link to="/recruit">Recruit</Link>
-    </div>
+      <Link to="signup">Join Now</Link>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default Employee;
