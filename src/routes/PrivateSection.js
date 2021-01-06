1b34da4075a843ec2030dbdacab0c05cd89f5498
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-06 11:21:50 -0800

    comment

--- src/routes/PrivateSection.js
+++ src/routes/PrivateSection.js
@@ -32,6 +32,7 @@ const StyledBlock = styled.div`
 function PrivateSection() {
   // conditional rendering based on react router
   let location = useLocation();
+  // todo: check state of login, would go to employer dashboard
   if (location.pathname.includes("/curriculum/")) {
     return (
       <div>
