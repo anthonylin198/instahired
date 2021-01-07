e9e27546b1ab887c56163a400f49977393166714
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-07 10:55:23 -0800

    image cleanup

--- src/routes/public/landing/employee/index.js
+++ src/routes/public/landing/employee/index.js
@@ -7,6 +7,10 @@ import styled from "styled-components";
 import Navbar from "../../../../components/public/Navbar";
 import Sidebar from "../../../../components/public/Sidebar";
 
+import student from "../../../../assets/img/student.jpg";
+// Material UI
+import Grid from "@material-ui/core/Grid";
+
 const Employee = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -18,11 +22,20 @@ const Employee = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar setIsOpen={setIsOpen} toggle={toggle} />
       <Container>
-        <h1>Skip to the Final Round Interview At Your Dream Startup</h1>
-        <h2>asdasdasdvfddsf</h2>
-        <Link to="/recruit">Recruit</Link>
-
-        <Link to="signup">Join Now</Link>
+        <LeftGrid item xs={12} md={6}>
+          {/* Content Container */}
+          <ContentContainer>
+            <h1>Instahired</h1>
+            <h2>Connecting Students to Startups</h2>
+            <Link to="/recruit">Recruit</Link>
+            <Link to="signup">Join Now</Link>
+          </ContentContainer>
+        </LeftGrid>
+        <RightGrid item xs={false} md={6}>
+          <ContentContainer>
+            <h1>here</h1>
+          </ContentContainer>
+        </RightGrid>
       </Container>
     </>
   );
@@ -32,10 +45,36 @@ const Container = styled.div`
   display: flex;
   height: 94vh;
   background: #f9f9f9;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 2rem;
 `;
 
+const ContentContainer = styled.div`
+  color: white;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  flex-direction: column;
+`;
+
+const LeftGrid = styled(Grid)`
+  background-color: #3179ff;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  text-align: left;
+`;
+
+const RightGrid = styled(Grid)`
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),
+    url(${student});
+  /* background-image: url(${student}); */
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  text-align: left;
+`;
 export default Employee;
