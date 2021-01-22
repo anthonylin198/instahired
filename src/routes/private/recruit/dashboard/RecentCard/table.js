da8db1d8ff1944f3023ce9928c1f34ec1405c894
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-22 08:35:23 -0800

    cleanup table data

--- src/routes/private/recruit/dashboard/RecentCard/table.js
+++ src/routes/private/recruit/dashboard/RecentCard/table.js
@@ -11,51 +11,51 @@ import TableRow from "@material-ui/core/TableRow";
 
 const columns = [
   { id: "name", label: "Name", minWidth: 170 },
-  { id: "job", label: "Job Applied", minWidth: 100 },
+  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
   {
-    id: "culture",
-    label: "Culture Fit Score",
+    id: "population",
+    label: "Population",
     minWidth: 170,
-    align: "center",
+    align: "right",
     format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: "technical",
-    label: "Technical Assessment",
+    id: "size",
+    label: "Size\u00a0(km\u00b2)",
     minWidth: 170,
-    align: "center",
+    align: "right",
     format: (value) => value.toLocaleString("en-US"),
   },
-  // {
-  //   id: "density",
-  //   label: "Density",
-  //   minWidth: 170,
-  //   align: "right",
-  //   format: (value) => value.toFixed(2),
-  // },
+  {
+    id: "density",
+    label: "Density",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
 ];
 
-function createData(name, job, culture, technical) {
-  // const density = population / size;
-  return { name, job, culture, technical };
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
 }
 
 const rows = [
-  createData("Anthony", "Frontend Engineer", "95%", 10),
-  createData("John", "Frontend Engineer", "70%", 9),
-  createData("Spenser", "Frontend Engineer", "88%", 10),
-  createData("Steven", "Business Intern", "92%", 7),
-  createData("Henry", "Business Intern", "45%", 8),
-  createData("Jill", "Backend Engineer", "93%", 5),
-  createData("Jane", "Business Intern", "77%", 9),
-  createData("Johnny", "Frontend Engineer", "85%", 8),
-  createData("Jason", "Backend Engineer", "88%", 6),
-  createData("Kevin", "Business Intern", "65%", 10),
-  createData("Trisha", "Frontend Engineer", "88%", 8),
-  createData("Tony", "Backend Engineer", "95%", 9),
-  createData("Johnny", "Business Intern", "45%", 7),
-  createData("Jake", "Frontend Engineer", "73%", 3),
-  createData("Jillian", "Backend Engineer", "90%", 8),
+  createData("India", "IN", 1324171354, 3287263),
+  createData("China", "CN", 1403500365, 9596961),
+  createData("Italy", "IT", 60483973, 301340),
+  createData("United States", "US", 327167434, 9833520),
+  createData("Canada", "CA", 37602103, 9984670),
+  createData("Australia", "AU", 25475400, 7692024),
+  createData("Germany", "DE", 83019200, 357578),
+  createData("Ireland", "IE", 4857000, 70273),
+  createData("Mexico", "MX", 126577691, 1972550),
+  createData("Japan", "JP", 126317000, 377973),
+  createData("France", "FR", 67022000, 640679),
+  createData("United Kingdom", "GB", 67545757, 242495),
+  createData("Russia", "RU", 146793744, 17098246),
+  createData("Nigeria", "NG", 200962417, 923768),
+  createData("Brazil", "BR", 210147125, 8515767),
 ];
 
 const useStyles = makeStyles({
@@ -63,7 +63,7 @@ const useStyles = makeStyles({
     width: "100%",
   },
   container: {
-    maxHeight: "33vh",
+    maxHeight: "25vh",
   },
 });
 
