6c44b78d24d4ecfe06e19505dac7de8332ad773b
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-24 10:09:23 -0800

    pages setup

--- src/routes/private/recruit/assessments/index.js
+++ src/routes/private/recruit/assessments/index.js
@@ -1,11 +1,44 @@
 import React from "react";
 
-const index = () => {
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const courses = [
+  { courseTitle: "assessment1" },
+  { courseTitle: "assessment2" },
+  { courseTitle: "assessment3" },
+  { courseTitle: "assessment4" },
+  { courseTitle: "assessment5" },
+  { courseTitle: "assessment6" },
+  { courseTitle: "assessment7" },
+];
+
+const Assessments = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Assessments</h1>
+    <div className={classes.root}>
+      {/* Your Progress */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={3}>
+          Create New Assessment
+        </Grid>
+        {/* map out the array with all the assessments company has */}
+        {courses.map((item, index) => {
+          return (
+            <Grid item xs={12} md={3}>
+              <div>{item.courseTitle}</div>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 };
 
-export default index;
+export default Assessments;
--- src/routes/private/recruit/openpositions/index.js
+++ src/routes/private/recruit/openpositions/index.js
@@ -1,11 +1,44 @@
 import React from "react";
 
-const index = () => {
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const courses = [
+  { courseTitle: "position1" },
+  { courseTitle: "posiiton2" },
+  { courseTitle: "position3" },
+  { courseTitle: "position4" },
+  { courseTitle: "position5" },
+  { courseTitle: "position6" },
+  { courseTitle: "position7" },
+];
+
+const Assessments = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Open Positions</h1>
+    <div className={classes.root}>
+      {/* Your Progress */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={3}>
+          Create New Assessment
+        </Grid>
+        {/* map out the array with all the assessments company has */}
+        {courses.map((item, index) => {
+          return (
+            <Grid item xs={12} md={3}>
+              <div>{item.courseTitle}</div>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 };
 
-export default index;
+export default Assessments;
