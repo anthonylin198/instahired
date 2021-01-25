5e2de5b40b7f720482c7f5cbbdd56765c6796b65
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-25 12:29:31 -0800

    fixup sidebar menu and job page

--- src/routes/private/employee/jobs/index.js
+++ src/routes/private/employee/jobs/index.js
@@ -21,7 +21,7 @@ const Dashboard = () => {
         <Grid item xs={12} md={2}>
           <FilterCard />
         </Grid>
-        <Grid item xs={12} md={8}>
+        <Grid item xs={12} md={7}>
           <p>Top Startup Jobs</p>
           <CompanyCard />
           <CompanyCard />
@@ -30,7 +30,7 @@ const Dashboard = () => {
           <CompanyCard />
         </Grid>
         {/* Need to make this more responsive, right now just no show on mobile */}
-        <Grid item xs={12} md={2}>
+        <Grid item xs={12} md={3}>
           <p>Your Profile Status</p>
         </Grid>
       </Grid>
