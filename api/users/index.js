5102d95438043211ee38255f4dca3a862296a0c2
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-28 08:31:14 -0800

    users fix

--- api/users/index.js
+++ api/users/index.js
@@ -15,14 +15,6 @@ module.exports = async (req, res) => {
         res.status(400).json({ success: false });
       }
       break;
-    case "POST":
-      try {
-        const user = await User.create(req.body);
-        res.status(201).json({ success: true, data: user });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
     default:
       res.status(400).json({ success: false });
       break;
